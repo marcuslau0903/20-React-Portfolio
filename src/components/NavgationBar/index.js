6975d1bde5f4333b2536eb7e7ed2304d9c1bb568
MarcusLau <marcuslau0903@gmail.com>
2021-08-15 12:57:53 +0100

    navbar

--- src/components/NavgationBar/index.js
+++ src/components/NavgationBar/index.js
@@ -1,5 +1,24 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+
 const NavigationBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Marcus Lau</Navbar.Brand>
+        <Nav className="me-auto">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/contact-me" className="nav-link">
+            Contact Me
+          </Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
